593613260b04c945b85188c1380366c4f14e5e1f
Jason Leach <jlspursfan@gmail.com>
2019-07-23 17:27:22 -0700

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const k_muppet = {
   k_color: 'green',
   k_album: {
     the_muppet_movie: {
-      song_1: 'Rainbow Connection',
+      song_1: 'The Rainbow Connection',
       song_2: 'Moving Right Along',
       song_3: 'Never Before, Never Again',
       song_4: 'I Hope That Something Better Comes Along',
@@ -28,22 +28,37 @@ const k_muppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+  const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-
+  const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(' ')
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-
+  const [black_and_white, , black, pink, ] = farmAnimals.split(' ')
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+  const [red, orange, yellow, green, blue, indigo, violet] = colors
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
+  const [r, o, y, g, b, , v] = colors
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+  const [ , , , , , indg, ] = colors
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
+  const name = k_muppet.k_name;
+  const color = k_muppet.k_color;
+  const song = k_muppet.k_album.the_muppet_movie.song_1
+  const job = k_muppet.k_job;
+  const partner = k_muppet.k_partner;
+  console.log(name);
+  console.log(color);
+  console.log(song);
+  console.log(job);
+  console.log(partner);
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+  const {song_2, song_4} = k_muppet.k_album.the_muppet_movie;
+  const {k_job, k_partner} = k_muppet;
+  console.log(song_2);
+  console.log(song_4);
+  console.log(k_job);
+  console.log(k_partner);
